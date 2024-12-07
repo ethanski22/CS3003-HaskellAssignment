4efbe21d905fba92bfe89f96b22705a01a75ecfb
Ethan Skowronski <ethanski22@icloud.com>
2024-12-07 13:17:15 -0500

    started homework

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -9,7 +9,13 @@ instance Show Found where
   show NoMatch = "No match found!"
 findFirst :: Eq a => (a -> Bool) -> [a] -> Found
 
+findFirst predicate = go 0
+  where
+    go _ [] = NoMatch
+    go index (x:xs) = if predicate x then Match index else go (index + 1) xs
+
 ------------------------------------------------
 -- palindrome
 ------------------------------------------------
 palindrome :: [Char] -> Bool
+palindrome str = str == reverse str
